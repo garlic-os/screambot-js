d764f1e5e2d0681098a11dcfdd90d7c2d852c88c
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-03-21 16:06:29 -0500

    Add Elastic Beanstalk compatibility
    Elastic Beanstalk has this design flaw where it blows up if there are any quotation marks (") in an environment variable. This makes it so that you can't pass normally-formatted JSON as an environment variable when running an app off Elastic Beanstalk. To get around this, this patch lets Screambot read JSON that uses the grave character (`) as quotation marks (") by reading graves as quotation marks when parsing them into JavaScript Objects. This way, you can finally use JSON in EB's environment variables (and by extension, run Screambot there!).
    
    This change does not break previous builds that use normal JSON. Normal JSON is still compatible with Screambot.
    If you want to run Screambot in Elastic Beanstalk, all you have to do is change the quotation marks (") in your JSON to graves (`); for example:
    '{"garlicOS":"206235904644349953"}'
    becomes:
    '{`garlicOS`:`206235904644349953`}'

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-"use strict"
-
 // Load environment variables to const config
 // JSON parse any value that is JSON parseable
 const config = require("./defaults")
 for (const key in process.env) {
 	try {
-		config[key] = JSON.parse(process.env[key])
+		// Replace ` with " for smuggling quotation marks into Elastic Beanstalk
+		config[key] = JSON.parse(process.env[key].replace("`", `"`))
 	} catch (e) {
 		config[key] = process.env[key]
 	}
